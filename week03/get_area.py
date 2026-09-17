466a33de2cb0aba6cfec6790d5c4a136fe26e3c2
25730026-beep <25730026@g.sewu.ac.kr>
2026-09-17 12:36:46 +0900

    Update get_area.py

--- week03/get_area.py
+++ week03/get_area.py
@@ -5,13 +5,13 @@ return area
 result = get_area(3)
 print("반지름이 3인 원의 면적=", result)
 
-
 a = [1,2,3]
 b = (1,2,3)
 
 def add(*numbers):
-sum = 0
-for i in numbers:
-print(type(numbers))
+   sum = sum + i
+ return sum
+ for i in numbers:
+    print(type(numbers))
 
 print(add(10, 20))
